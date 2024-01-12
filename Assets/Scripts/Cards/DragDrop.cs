234a6c6bd080d0b2bbafe506fba64267f7aec681
DanielZapadakovec <poliacekdaniel999@gmail.com>
2024-01-12 08:25:48 +0100

    newversion

---
+++ Assets/Scripts/Cards/DragDrop.cs
@@ -0,0 +1,169 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    public GameObject Canvas;
+
+    private bool isDragging = false;
+    private Vector2 startPosition;
+    public static GameObject dropZone;
+    private bool isDraggable = true;
+    private bool isOverDropZone = false;
+    private GameObject startParent;
+    public  Vector2 deckposition;
+    public DrawCards drawCards;
+    public List<GameObject> topThree = new List<GameObject>();
+
+    void Start()
+    {
+        Canvas = GameObject.Find("MainCanvas");
+        drawCards = GameObject.Find("DrawCardButton").GetComponent<DrawCards>();
+    
+
+    }
+    void Update()
+    {
+        
+
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        dropZone = null;
+    }
+
+
+    public void StartDrag()
+    {
+        if (!isDraggable) return;
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
+        isDragging = true;
+
+    }
+    public void EndDrag()
+    {
+        isDragging = false;
+        if (isOverDropZone == true) 
+        {
+
+            dropZone.GetComponent<DropZone>().AddToDiscardPile(gameObject);
+
+            if (DrawCards.drawablecardforPlayer)
+            {
+                DrawCards.cardsPlayerDeck.Remove(gameObject);
+            }
+            else if (DrawCards.drawablecardforEnemy)
+            {
+                DrawCards.cardsEnemyDeck.Remove(gameObject);
+                    
+            }
+
+            transform.SetParent(dropZone.transform, true);
+            isDraggable = false;
+            TestingCard();
+        }
+        else
+        {
+            transform.position = startPosition;
+            transform.localScale = Vector3.one;
+            transform.SetParent(startParent.transform, false);
+        }
+    }
+
+    public void TestingCard()
+    {
+        int cardId = GetComponent<CardIdentity>().ID;
+        if (cardId == 0 && DrawCards.drawablecardforPlayer)
+        {
+            Debug.Log("Karta s ID 0 je v dropzóne. Vykonávam špecifickú akciu.");
+        }
+        else if (cardId == 0 && DrawCards.drawablecardforEnemy)
+        {
+            Debug.Log("Karta s ID 0 je v dropzóne. Vykonávam špecifickú akciu.");
+        }
+        else if (cardId == 2 && DrawCards.drawablecardforPlayer)
+        {
+            drawCards.CanBeDrawed = false;
+            Debug.Log("Hráè si pozrie prvé tri karty z balíèka.");
+            for (int i = 0; i < 3 && i < drawCards.cardsInDeck.Count; i++)
+            {
+                topThree.Add(drawCards.cardsInDeck[i]);
+                GameObject card = Instantiate(drawCards.cardsInDeck[drawCards.cardsInDeck.Count - 1], new Vector2(0, 0), Quaternion.identity);
+                card.transform.SetParent(drawCards.ViewPlayerArea.transform, false);
+            }
+
+        }
+        else if (cardId == 2 && DrawCards.drawablecardforEnemy)
+        {
+            Debug.Log("Hráè si pozrie prvé tri karty z balíèka.");
+        }
+        else if (cardId == 3 && DrawCards.drawablecardforPlayer)
+        {
+            Debug.Log("Karta s ID 0 je v dropzóne. Vykonávam špecifickú akciu.");
+        }
+        else if (cardId == 3 && DrawCards.drawablecardforEnemy)
+        {
+            Debug.Log("Karta s ID 0 je v dropzóne. Vykonávam špecifickú akciu.");
+        }
+        else if (cardId == 4 && DrawCards.drawablecardforPlayer)
+        {
+            Debug.Log("Karta s ID 0 je v dropzóne. Vykonávam špecifickú akciu.");
+        }
+        else if (cardId == 4 && DrawCards.drawablecardforEnemy)
+        {
+            Debug.Log("Karta s ID 0 je v dropzóne. Vykonávam špecifickú akciu.");
+        }
+        else if (cardId == 5 && DrawCards.drawablecardforPlayer)
+        {
+            Debug.Log("Karty boli zamiešane");
+            drawCards.ShuffleCardsInDeck();
+        }
+        else if (cardId == 5 && DrawCards.drawablecardforEnemy)
+        {
+            Debug.Log("Karty boli zamiešane");
+            drawCards.ShuffleCardsInDeck();
+        }
+        else if (cardId == 6 && DrawCards.drawablecardforPlayer)
+        {
+            DrawCards.drawablecardforPlayer = false;
+            DrawCards.drawablecardforEnemy = true;
+        }
+        else if (cardId == 6 && DrawCards.drawablecardforEnemy)
+        {
+            DrawCards.drawablecardforPlayer = true;
+            DrawCards.drawablecardforEnemy = false;
+        }
+        else if (cardId == 7 && DrawCards.drawablecardforPlayer)
+        {
+        }
+        else if (cardId == 7 && DrawCards.drawablecardforEnemy)
+        {
+
+        }
+    }
+
+
+    public void ReturnCardsToDeck()
+    {
+        for (int i = topThree.Count - 1; i >= 0; i--)
+        {
+            drawCards.cardsInDeck.Insert(0, topThree[i]);
+        }
+        drawCards.CanBeDrawed = true;
+    }
+}
+
---
+++ Assets/Scripts/Cards/DrawCards.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public List<GameObject> cardsInDeck = new List<GameObject>();
+    public GameObject PlayerArea;
+    public GameObject ViewPlayerArea;
+    public static List<GameObject> cardsPlayerDeck = new List<GameObject>();
+    public static List<GameObject> cardsEnemyDeck = new List<GameObject>();
+    public bool StartGameWasExecuted;
+    public static bool drawablecardforPlayer = true;
+    public static bool drawablecardforEnemy;
+    public GameObject EnemyArea;
+    public GameObject ViewEnemyArea;
+    public GameObject vychovavatelprefab;
+    public GameObject BackOfCardPrefab;
+    public GameManager gameManager;
+    public bool CanBeDrawed = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartGame());
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        CanBeDrawed = true;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (drawablecardforEnemy == true )
+        {
+            ShowBackOfCard(cardsEnemyDeck, false);
+            ShowBackOfCardforPlayer(cardsPlayerDeck, true);
+        }
+        else if (drawablecardforPlayer == true )
+        {
+            ShowBackOfCard(cardsEnemyDeck, true);
+            ShowBackOfCardforPlayer(cardsPlayerDeck, false);
+        }
+    }
+    
+    public void OnClick()
+    {
+        if (CanBeDrawed == true)
+        {
+            if (cardsInDeck.Count > 0 && drawablecardforPlayer == true)
+            {
+                GameObject card = Instantiate(cardsInDeck[cardsInDeck.Count - 1], new Vector2(0, 0), Quaternion.identity);
+                card.transform.SetParent(PlayerArea.transform, false);
+
+                cardsPlayerDeck.Add(card);
+
+                cardsInDeck.RemoveAt(cardsInDeck.Count - 1);
+                if (gameManager.hasCardWithID0 == false)
+                {
+
+                }
+                drawablecardforPlayer = false;
+                drawablecardforEnemy = true;
+            }
+            else if (cardsInDeck.Count > 0 && drawablecardforEnemy == true)
+            {
+                GameObject card = Instantiate(cardsInDeck[cardsInDeck.Count - 1], new Vector2(0, 0), Quaternion.identity);
+                card.transform.SetParent(EnemyArea.transform, false);
+
+                cardsEnemyDeck.Add(card);
+
+                cardsInDeck.RemoveAt(cardsInDeck.Count - 1);
+
+                drawablecardforPlayer = true;
+                drawablecardforEnemy = false;
+            }
+            else if (cardsPlayerDeck.Count < 1)
+            {
+                Debug.Log("Balíèek je prázdny.");
+            }
+        }
+
+    }
+    public void ShuffleCardsInDeck()
+    {
+        int n = cardsInDeck.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            GameObject value = cardsInDeck[k];
+            cardsInDeck[k] = cardsInDeck[n];
+            cardsInDeck[n] = value;
+        }
+    }
+
+    IEnumerator StartGame()
+    {
+        ShuffleCardsInDeck();
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject card = Instantiate(cardsInDeck[cardsInDeck.Count - 1], new Vector2(0, 0), Quaternion.identity);
+            card.transform.SetParent(PlayerArea.transform, false);
+
+            cardsPlayerDeck.Add(card);
+
+            cardsInDeck.RemoveAt(cardsInDeck.Count - 1);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject card = Instantiate(cardsInDeck[cardsInDeck.Count - 1], new Vector2(0, 0), Quaternion.identity);
+            card.transform.SetParent(EnemyArea.transform, false);
+
+            cardsEnemyDeck.Add(card);
+
+            cardsInDeck.RemoveAt(cardsInDeck.Count - 1);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+        for (int i = 0; i < 1; i++) //tu do budúcna, sa dá že spawne iba tolko vychovavatelov kolko je hráèov -1
+        {
+            cardsInDeck.Add(vychovavatelprefab);
+        }
+        ShuffleCardsInDeck();
+        StartGameWasExecuted = true;
+
+    }
+    void ShowBackOfCard(List<GameObject> cardsEnemyDeck, bool showBack)
+    {
+        foreach (var card in cardsEnemyDeck)
+        {
+            Transform backOfCard = card.transform.Find("CardBack");
+            if (backOfCard != null)
+            {
+                backOfCard.gameObject.SetActive(showBack);
+            }
+        }
+    }
+    void ShowBackOfCardforPlayer(List<GameObject> cardsPlayerDeck, bool showBack)
+    {
+        foreach (var card in cardsPlayerDeck)
+        {
+            Transform backOfCard = card.transform.Find("CardBack");
+            if (backOfCard != null)
+            {
+                backOfCard.gameObject.SetActive(showBack);
+            }
+        }
+    }
+
+
+
+}
