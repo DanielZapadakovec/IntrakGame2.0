7c47be1f9a6696e661cb13ceec7ee1d616166c20
DanielZapadakovec <poliacekdaniel999@gmail.com>
2024-01-16 12:23:36 +0100

    Update 0.13
    Only not saved version, now it´s saved

--- Assets/Scripts/EnemySiteScript.cs
+++ Assets/Scripts/EnemySiteScript.cs
@@ -24,7 +24,6 @@ public class EnemySiteScript : MonoBehaviour
         {
             transform.position = startPosition;
         }
-        // Nastavenie nových pozícií objektu
-        transform.position = targetPosition;
+
     }
 }
